1ec5980fea53f749a500ec2d754809426848c0fc
1946673 <1946673@01-19852.pusd.dom>
2022-01-25 12:28:55 -0800

    activity 1

--- 2048 Student Version 2021/src/Activity1Tester.java
+++ 2048 Student Version 2021/src/Activity1Tester.java
@@ -4,13 +4,13 @@ public class Activity1Tester {
 	public static void main(String[] args) {
 		
 		// Create a Board object
-		Board b = new Board();
+		Board myBoard = new Board();
 		
 		// invoke the toString method by passing the object to 
 		// print or println
 
 		
-		System.out.println(b);
+		System.out.println(myBoard);
 		
 	}
 
